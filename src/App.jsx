6823d8e604b37f8a84505768b4208d26c4b86af2
Nirupam Pal <nirupampal14@gmail.com>
2025-07-19 20:31:17 +0530

    Login initialized

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import Login from './components/Auth/login.jsx'
-import EmployeeDashboard from './components/Dashboard/EmployeeDashboard.jsx'
-import AdminDashboard from './components/Dashboard/AdminDashboard.jsx'
-import { getLocalStorage, setLocalStorage } from './utils/localStorage.jsx'
+import React, { useEffect, useState } from "react";
+import Login from "./components/Auth/login.jsx";
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard.jsx";
+import AdminDashboard from "./components/Dashboard/AdminDashboard.jsx";
+import { getLocalStorage, setLocalStorage } from "./utils/localStorage.jsx";
 
 const App = () => {
+  const [user, setUser] = useState(null);
 
-  
-
-  const [user, setUser] = useState(null)
-
-  const handleLogin = (email, password) =>{
-  }
+  const handleLogin = (email, password) => {
+    if (email == "admin@me.com" && password == "123") {
+      setUser("admin");
+      console.log(user);
+    } else if (email == "user@me.com" && password == "123") {
+      setUser("employee");
+      console.log(user);
+    } else {
+      alert("Invalid Credentials");
+    }
+  };
 
   return (
     <>
-      {!user ? <Login/>: ''}
-      {/* <EmployeeDashboard/> */}
-      {/* <AdminDashboard/> */}
+      {!user && <Login handleLogin={handleLogin} />}
+      {user === "admin" && <AdminDashboard />}
+      {user === "employee" && <EmployeeDashboard />}{" "}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
