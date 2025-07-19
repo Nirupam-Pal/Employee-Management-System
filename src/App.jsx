b3b1b50fa9391413c644c3594deb2938e1de0679
Nirupam Pal <nirupampal14@gmail.com>
2025-07-19 20:02:48 +0530

    Added datas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Login from './components/Auth/login.jsx'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard.jsx'
 import AdminDashboard from './components/Dashboard/AdminDashboard.jsx'
+import { getLocalStorage, setLocalStorage } from './utils/localStorage.jsx'
 
 const App = () => {
+
+  
+
+  const [user, setUser] = useState(null)
+
+  const handleLogin = (email, password) =>{
+  }
+
   return (
     <>
-      {/* <Login/> */}
-      <EmployeeDashboard/>
+      {!user ? <Login/>: ''}
+      {/* <EmployeeDashboard/> */}
       {/* <AdminDashboard/> */}
     </>
   )
