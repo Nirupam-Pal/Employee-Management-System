6da7ba4e2746748cf9327b605a79a1dd81208ce1
Nirupam Pal <nirupampal14@gmail.com>
2025-07-20 17:38:51 +0530

    Task list numbers implemented

--- src/components/Other/Header.jsx
+++ src/components/Other/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Header = ({data}) => {
+  console.log(data);
+  
   return (
     <div className="flex items-end text-white justify-between">
       <h1 className="text-2xl font-medium">
-        Hello <br /> <span className="text-3xl font-semibold">Nirupam</span>{" "}
+        Hello <br /> <span className="text-3xl font-semibold">{data.firstName}</span>{" "}
       </h1>
       <button className="bg-red-500 text-lg font-medium text-white px-5 py-2 rounded-sm">Log Out</button>
     </div>
